e6096db385cebfce3ba2cf433673b124efe80425
George Gordon <117164117+Flashggordon@users.noreply.github.com>
2023-11-07 14:58:40 +0000

    header complete

---
+++ flashcards/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from './logo512.png';
+export default function Header (){
+    return (
+        <div className="header">
+        <img className="header--logo" src={logo} alt="Image of logo" />
+        <h1 className="header--title">React Flashcards</h1>
+        <p className="header--subtitle"> 🧠Expand Your React Knoledge, One Flashcard at a Time!🐌 </p>
+       </div>
+    )
+    
+   
+
+
+}
